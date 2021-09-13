7341b4bddc2f86dc4009cbde80a0fd972767ea8f
ambacc244netand <ambacc244@netand.co.kr>
2021-09-13 17:02:24 +0900

    create nav folder folding button

--- src/components/Nav/IamNav.js
+++ src/components/Nav/IamNav.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {_NavContainer} from '../../styles/components/style';
 import {Link} from 'react-router-dom';
 
 const IamNav = () => {
+	const [isOpened, setIsOpend] = useState(false);
+
+	const onClickFoldOrUnFoldFolder = useCallback(() => {
+		setIsOpend(!isOpened);
+	}, [isOpened]);
 	return (
 		<_NavContainer>
 			<div>대시보드</div>
-			<Link to='/user'>사용자</Link>
-			<Link to='/group'>사용자 그룹</Link>
-			<Link to='/role'>역할</Link>
-			<Link to='/policy'>정책</Link>
+			<div>
+				접근 관리 <button onClick={onClickFoldOrUnFoldFolder}>B</button>
+			</div>
+			{isOpened && (
+				<_NavContainer>
+					<Link to='/user'>사용자</Link>
+					<Link to='/group'>사용자 그룹</Link>
+					<Link to='/role'>역할</Link>
+					<Link to='/policy'>정책</Link>
+				</_NavContainer>
+			)}
 		</_NavContainer>
 	);
 };
