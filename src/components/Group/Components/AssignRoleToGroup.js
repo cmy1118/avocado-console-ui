d979d2abf3b25cf63839a90e81fd6b0055b48743
ambacc244netand <ambacc244@netand.co.kr>
2021-10-19 17:13:25 +0900

    edit

--- src/components/Group/Components/AssignRoleToGroup.js
+++ src/components/Group/Components/AssignRoleToGroup.js
@@ -24,7 +24,7 @@ const AssignRoleToGroup = ({addedRoles, setAddedRoles}) => {
 			.map((v) => ({
 				...v,
 				type: roleTypeConverter(v.companyId),
-				numberOfGroups: v.groups.length,
+				numberOfUsers: v.users.length,
 			}));
 	}, [roles, addedRoles]);
 
