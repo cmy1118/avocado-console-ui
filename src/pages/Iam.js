b589b2ad136f17c4e414b0423c4a374d7d721b48
ambacc244netand <ambacc244@netand.co.kr>
2021-11-09 14:31:29 +0900

    solve backgound image fade

--- src/pages/Iam.js
+++ src/pages/Iam.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import dashboard from '../images/backgound/dashboard.png';
+import dashboard from '../images/backgound/dashboard@2x.png';
 
 const _Container = styled.div`
 	margin-top: 54px;
@@ -10,6 +10,11 @@ const _Container = styled.div`
 	height: 100%;
 	width: 100%;
 	background-size: cover;
+	image-rendering: -moz-crisp-edges;
+	mage-rendering: -o-crisp-edges;
+	image-rendering: -webkit-optimize-contrast;
+	-ms-interpolation-mode: nearest-neighbor;
+	image-rendering: crisp-edges;
 `;
 
 const Iam = () => {
