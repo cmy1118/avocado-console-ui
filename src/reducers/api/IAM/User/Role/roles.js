81891f9dc0e37163dac71091f6e2e88de3e2ffdb
seob717 <seob717@netand.co.kr>
2021-11-18 11:26:51 +0900

    iam role api 작성

--- src/reducers/api/IAM/User/Role/roles.js
+++ src/reducers/api/IAM/User/Role/roles.js
@@ -1,144 +1,208 @@
-import {createSelector, createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSelector, createSlice} from '@reduxjs/toolkit';
+import axios from 'axios';
+import {baseUrl} from '../../../../../api/constants';
 
 const NAME = 'IAM_ROLES';
+
+const createAction = createAsyncThunk(
+	`${NAME}/CREATE`,
+	async (payload, {getState}) => {
+		const {user} = getState().AUTH_USER;
+		// eslint-disable-next-line no-console
+		console.log(user);
+		const response = await axios.post(
+			`/open-api/v1/iam/roles`,
+			{
+				name: payload.name,
+				description: payload.description,
+				maxGrants: payload.maxGrants, //최대 승인 수 0 : 제한없음 N: 부여 가능 수
+				parentIds: payload.parentIds,
+			},
+			{
+				headers: {
+					Authorization: `${user.token_type} ${user.access_token}`,
+					'Content-Type': 'application/json',
+				},
+				baseURL: baseUrl.openApi,
+			},
+		);
+		return response.data;
+	},
+);
+
+const updateAction = createAsyncThunk(
+	`${NAME}/UPDATE`,
+	async (payload, {getState}) => {
+		const {user} = getState().AUTH_USER;
+		// eslint-disable-next-line no-console
+		console.log(user);
+		const response = await axios.put(
+			`/open-api/v1/iam/roles/${payload.id}`,
+			{
+				name: payload.name,
+				description: payload.description,
+				maxGrants: payload.maxGrants, //최대 승인 수 0 : 제한없음 N: 부여 가능 수
+			},
+			{
+				headers: {
+					Authorization: `${user.token_type} ${user.access_token}`,
+					'Content-Type': 'application/json',
+				},
+				baseURL: baseUrl.openApi,
+			},
+		);
+		return response.data;
+	},
+);
+
+const deleteAction = createAsyncThunk(
+	`${NAME}/DELETE`,
+	async (payload, {getState}) => {
+		const {user} = getState().AUTH_USER;
+		// eslint-disable-next-line no-console
+		console.log(user);
+		const response = await axios.delete(
+			`/open-api/v1/iam/roles/${payload.id}`,
+			{
+				headers: {
+					Authorization: `${user.token_type} ${user.access_token}`,
+				},
+				baseURL: baseUrl.openApi,
+			},
+		);
+		return response.data;
+	},
+);
+
+const findByIdAction = createAsyncThunk(
+	`${NAME}/FIND_BY_ID`,
+	async (payload, {getState}) => {
+		const {user} = getState().AUTH_USER;
+		// eslint-disable-next-line no-console
+		console.log(user);
+		const response = await axios.get(
+			`/open-api/v1/iam/roles/${payload.id}`,
+			{
+				headers: {
+					Authorization: `${user.token_type} ${user.access_token}`,
+				},
+				baseURL: baseUrl.openApi,
+			},
+		);
+		return response.data;
+	},
+);
+const getsAction = createAsyncThunk(
+	`${NAME}/GETS`,
+	async (payload, {getState}) => {
+		const {user} = getState().AUTH_USER;
+		// eslint-disable-next-line no-console
+		console.log(user);
+		const response = await axios.get(`/open-api/v1/iam/roles`, {
+			headers: {
+				Authorization: `${user.token_type} ${user.access_token}`,
+				Range: payload.range,
+			},
+			params: {
+				keyword: payload.keyword,
+				maxGrants: payload.maxGrants,
+			},
+			baseURL: baseUrl.openApi,
+		});
+		return response.data;
+	},
+);
+
+const getEventsAction = createAsyncThunk(
+	`${NAME}/GETS`,
+	async (payload, {getState}) => {
+		const {user} = getState().AUTH_USER;
+		// eslint-disable-next-line no-console
+		console.log(user);
+		const response = await axios.get(`/open-api/v1/iam/roles/events`, {
+			headers: {
+				Authorization: `${user.token_type} ${user.access_token}`,
+				Range: payload.range,
+			},
+			params: {
+				fromTime: payload.fromTime,
+				toTime: payload.toTime,
+				roleld: payload.roleld,
+			},
+			baseURL: baseUrl.openApi,
+		});
+		return response.data;
+	},
+);
 
 const slice = createSlice({
 	name: NAME,
 	initialState: {
-		roleIndex: 0,
-		roles: [
-			{
-				id: 'role1',
-				name: 'manager-role',
-				description:
-					'사용자 레벨의 최고 관리자인 Manager에게 부여된 역할 (기본 제공)',
-				type: 'Public',
-				companyId: null,
-				users: ['user1', 'user2', 'user5', 'user10'],
-				groups: ['group1', 'group9'],
-				policies: ['policy1', 'policy2', 'policy3'],
-				creationDate: '2019.01.02 11:32:29',
-			},
-			{
-				id: 'role2',
-				name: 'admin-role',
-				description: 'Admin 사용자에게 부여 하는 역할',
-				type: 'Public',
-				companyId: null,
-				users: ['user1', 'user2', 'user7'],
-				groups: ['group1'],
-				policies: ['policy1', 'policy2'],
-				creationDate: '2019.01.04 15:12:40',
-			},
-			{
-				id: 'role3',
-				name: 'user-role',
-				description: '일반 User에게 부여 하는 역할',
-				type: 'Public',
-				users: ['user3', 'user8', 'user9', 'user10'],
-				groups: ['group4'],
-				companyId: null,
-				policies: [
-					'policy1',
-					'policy2',
-					'policy3',
-					'policy4',
-					'policy5',
-					'policy6',
-					'policy7',
-				],
-				creationDate: '2019.07.22 16:26:12',
-			},
-			{
-				id: 'role4',
-				name: 'guest-role',
-				description: 'Guest 사용자에게 부여하는 역할',
-				type: 'Public',
-				companyId: null,
-				users: ['user2', 'user9'],
-				groups: ['group4'],
-				policies: [
-					'policy1',
-					'policy2',
-					'policy3',
-					'policy4',
-					'policy5',
-				],
-				creationDate: '2021.02.21 20:05:48',
-			},
-			{
-				id: 'role5',
-				name: 'developerOnly-role',
-				description: '개발자에게 부여하기 위한 기본 역할',
-				type: 'Private',
-				companyId: null,
-				users: ['user3'],
-				groups: ['group3', 'group6'],
-				policies: ['policy1'],
-				creationDate: '2020.06.16 19:43:29',
-			},
-			{
-				id: 'role6',
-				name: 'operation-role',
-				description: '운영자에게 부여하기 위한 기본 역할',
-				type: 'Public',
-				companyId: null,
-				users: ['user3', 'user5', 'user7'],
-				groups: ['group2', 'group8'],
-				policies: [],
-				creationDate: '2021.08.07 09:41:23',
-			},
-		],
+		roles: [],
+		loading: false,
+		error: null,
 	},
-	reducers: {
-		addRolesToUser: (state, action) => {
-			const roles = state.roles.filter((v) =>
-				action.payload.roles.includes(v.id),
-			);
-
-			roles.map((v) => {
-				v.users.push(action.payload.uid);
-				return v;
-			});
+	reducers: {},
+	extraReducers: {
+		[createAction.pending]: (state) => {
+			state.loading = true;
 		},
-
-		deleteRolesFromUser: (state, action) => {
-			const roles = state.roles.filter((v) =>
-				action.payload.roles.includes(v.id),
-			);
-
-			roles.map((v) => {
-				const index = v.users.findIndex(
-					(val) => val === action.payload.uid,
-				);
-				v.users.splice(index, 1);
-				return v;
-			});
+		[createAction.fulfilled]: (state, action) => {
+			state.loading = false;
 		},
-
-		addRolesToGroup: (state, action) => {
-			const roles = state.roles.filter((v) =>
-				action.payload.roles.includes(v.id),
-			);
-
-			roles.map((v) => {
-				v.groups.push(action.payload.id);
-				return v;
-			});
+		[createAction.rejected]: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
 		},
-
-		deleteRolesFromGroup: (state, action) => {
-			const roles = state.roles.filter((v) =>
-				action.payload.roles.includes(v.id),
-			);
-
-			roles.map((v) => {
-				const index = v.groups.findIndex(
-					(val) => val === action.payload.id,
-				);
-				v.groups.splice(index, 1);
-				return v;
-			});
+		[updateAction.pending]: (state) => {
+			state.loading = true;
+		},
+		[updateAction.fulfilled]: (state, action) => {
+			state.loading = false;
+		},
+		[updateAction.rejected]: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
+		},
+		[deleteAction.pending]: (state) => {
+			state.loading = true;
+		},
+		[deleteAction.fulfilled]: (state, action) => {
+			state.loading = false;
+		},
+		[deleteAction.rejected]: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
+		},
+		[findByIdAction.pending]: (state) => {
+			state.loading = true;
+		},
+		[findByIdAction.fulfilled]: (state, action) => {
+			state.loading = false;
+		},
+		[findByIdAction.rejected]: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
+		},
+		[getsAction.pending]: (state) => {
+			state.loading = true;
+		},
+		[getsAction.fulfilled]: (state, action) => {
+			state.loading = false;
+		},
+		[getsAction.rejected]: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
+		},
+		[getEventsAction.pending]: (state) => {
+			state.loading = true;
+		},
+		[getEventsAction.fulfilled]: (state, action) => {
+			state.loading = false;
+		},
+		[getEventsAction.rejected]: (state, action) => {
+			state.error = action.payload;
+			state.loading = false;
 		},
 	},
 });
@@ -157,6 +221,14 @@ const IAM_ROLES = {
 	reducer: slice.reducer,
 	selector: (state) => selectAllState(state[slice.name]),
 	action: slice.actions,
+	asyncAction: {
+		createAction,
+		updateAction,
+		deleteAction,
+		findByIdAction,
+		getsAction,
+		getEventsAction,
+	},
 };
 
 export default IAM_ROLES;
