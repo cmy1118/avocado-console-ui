708daaf52588ce25b9a58be2924430a65fed5a56
roberto6385 <roberto@netand.co.kr>
2021-11-24 11:39:15 +0900

    "API_update : Bastion 경로추가"

--- src/components/Dasnboard/contentsButton.js
+++ src/components/Dasnboard/contentsButton.js
@@ -1,81 +1,74 @@
 import React, {useCallback} from 'react';
-import PropTypes from "prop-types";
-import AssignRoleToUser from "../IAM/User/Components/AssignRoleToUser";
-import styled from "styled-components";
+import PropTypes from 'prop-types';
+import AssignRoleToUser from '../IAM/User/Components/AssignRoleToUser';
+import styled from 'styled-components';
+import {useHistory} from 'react-router-dom';
 
-const _Container = styled.div`
-
-`
+const _Container = styled.div``;
 const _imgContainer = styled.div`
-position: relative;
-width:252px;
-height:240px;
-`
+	position: relative;
+	width: 252px;
+	height: 240px;
+`;
 const _textContents = styled.div`
-position: absolute;
-      top: 50%;
-    left: 50%;
-transform: translate( -50%, 100% );
-`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, 100%);
+`;
 const _title = styled.div`
-  font-family: NotoSansCJKKR;
-  font-size: 18px;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.11;
-  letter-spacing: 0.25px;
-  text-align: center;
-  color: #fff;
-  margin-bottom:5px;
-   text-shadow: 0.5px 0.5px 0.5px black;
-  
-`
+	font-family: NotoSansCJKKR;
+	font-size: 18px;
+	font-weight: bold;
+	font-stretch: normal;
+	font-style: normal;
+	line-height: 1.11;
+	letter-spacing: 0.25px;
+	text-align: center;
+	color: #fff;
+	margin-bottom: 5px;
+	// text-shadow: 0.5px 0.5px 0.5px black;
+`;
 const _text = styled.div`
-  font-family: NotoSansCJKKR;
-  font-size: 12px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.67;
-  letter-spacing: 0.25px;
-  text-align: center;
-  color: #fff;
-     text-shadow: 0.5px 0.5px 0.5px black;
-
-`
-
+	font-family: NotoSansCJKKR;
+	font-size: 12px;
+	font-weight: normal;
+	font-stretch: normal;
+	font-style: normal;
+	line-height: 1.67;
+	letter-spacing: 0.25px;
+	text-align: center;
+	color: #fff;
+	// text-shadow: 0.5px 0.5px 0.5px black;
+`;
 
 const ContentsButton = ({img, title, text}) => {
-    console.log('img:',img);
-    console.log('title:',title);
-    console.log('text:',text);
-    const onClickMove = useCallback(() => {
-        history.push('/iam');
-    }, [history]);
+	const history = useHistory();
 
-    return (
-        <_Container>
-            <_imgContainer>
-            <img style={{width:"252px",height:"240px"}} src={img}  />
-            {/*<img style={{width:"252px",height:"240px"}} src={img} onClick={onClickMove} />*/}
-                <_textContents>
-                    <_title>
-                        {title}
-                    </_title>
-                   <_text>
-                       {text}
-                   </_text>
-                </_textContents>
-            </_imgContainer>
+	const onClickMove = useCallback(() => {
+		history.push('./ssh://');
+		// history.push('/iam');
+	}, [history]);
 
-        </_Container>
-    );
+	return (
+		<_Container>
+			<a href={'ssh://'}>
+				<_imgContainer>
+					<img style={{width: '252px', height: '240px'}} src={img} />
+					{/*<img style={{width:"252px",height:"240px"}} src={img} onClick={onClickMove} />*/}
+					<_textContents>
+						<_title>{title}</_title>
+						<_text>{text}</_text>
+					</_textContents>
+				</_imgContainer>
+			</a>
+		</_Container>
+	);
 };
 ContentsButton.propTypes = {
-    img: PropTypes.string,
-    title: PropTypes.string,
-    text: PropTypes.string,
+	img: PropTypes.string,
+	title: PropTypes.string,
+	text: PropTypes.string,
 };
 
 export default ContentsButton;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,23 +2,38 @@ import React, {useCallback} from 'react';
 import {account} from '../utils/auth';
 import kt from '../images/backgound/workplace-2@2x.png';
 import samsung from '../images/backgound/workplce@2x.png';
-import img from '../images/dashboard/service_app_gray@2x.png'
+import img from '../images/dashboard/service_app_gray@2x.png';
 import {useSelector} from 'react-redux';
 import AUTH_USER from '../reducers/api/Auth/authUser';
 import {useHistory} from 'react-router-dom';
 import styled from 'styled-components';
-import ContentsButton from "../components/Dasnboard/contentsButton";
-import {status} from "../utils/data";
+import ContentsButton from '../components/Dasnboard/contentsButton';
+import {status} from '../utils/data';
 //img
-import IAM_green from '../images/dashboard/service_iam_green@2x.png'
-import PAM_green from '../images/dashboard/service_pam_green@2x.png'
-import Approal_green from '../images/dashboard/service_app_green@2x.png'
+import IAM_green from '../images/dashboard/service_iam_green@2x.png';
+import PAM_green from '../images/dashboard/service_pam_green@2x.png';
+import Approal_green from '../images/dashboard/service_app_green@2x.png';
 
-const favorite =[
-	{title:'IAM',text:'신원 및 접근 관리',img:IAM_green},
-	{title:'PAM',text:'특권 접근 관리',img:PAM_green},
-	{title:'Approal',text:'승인 워크플로우 관리',img:Approal_green},
-]
+// import Approal_green from '../images/dashboard/service_app_green@2x.png';
+// import Approal_green from '../images/dashboard/service_app_green@2x.png';
+// import Approal_green from '../images/dashboard/service_app_green@2x.png';
+
+const utils = [
+	{title: 'IAM', text: '신원 및 접근 관리', img: IAM_green},
+	{title: 'PAM', text: '특권 접근 관리', img: PAM_green},
+];
+const favorite = [
+	{title: 'IAM', text: '신원 및 접근 관리', img: IAM_green},
+	{title: 'PAM', text: '특권 접근 관리', img: PAM_green},
+	{title: 'Approal', text: '승인 워크플로우 관리', img: Approal_green},
+];
+const service = [
+	{title: 'IAM', text: '신원 및 접근 관리', img: IAM_green},
+	{title: 'PAM', text: '특권 접근 관리', img: PAM_green},
+	{title: 'Approal', text: '승인 워크플로우 관리', img: Approal_green},
+	{title: 'Approal', text: '승인 워크플로우 관리', img: Approal_green},
+	{title: 'Approal', text: '승인 워크플로우 관리', img: Approal_green},
+];
 
 // const _Container = styled.div`
 // 	display: grid;
@@ -31,20 +46,18 @@ const favorite =[
 // `;
 const _Container = styled.div`
 	display: grid;
-	height:955px;
-    grid-template-rows: 100%;
-	  grid-template-columns: 10% 80% 10%;
+	height: 955px;
+	grid-template-rows: 100%;
+	grid-template-columns: 10% 80% 10%;
 	height: 100%;
 	background: #f8f9fa;
-	 width: 100%;
-    grid-auto-rows: 40px; 
+	width: 100%;
+	grid-auto-rows: 40px;
 
- overflow-y: auto;
- &::-webkit-scrollbar {
-    display: none; 
-}
-
-
+	overflow-y: auto;
+	&::-webkit-scrollbar {
+		display: none;
+	}
 `;
 
 // const _ImageContainer = styled.div`
@@ -67,24 +80,21 @@ const _Container = styled.div`
 // `;
 
 const _contentsContainer = styled.div`
-padding: 55px 0px;
-// display: flex;
+	padding: 55px 0px;
+	// display: flex;
 	// align-items: start;
 	// justify-content: center;
 	// overflow: hidden;
-width:100%;
-height:100%
-`
+	width: 100%;
+	height: 100%;
+`;
 
 const _contents = styled.div`
-display:flex;
-width:100%;
-height:300px;
-    `
-const _header =styled.div`
-`
-const _footer =styled.div`
-`
+	display: flex;
+	width: 100%;
+	height: 300px;
+`;
+const _side = styled.div``;
 
 const Main = () => {
 	const history = useHistory();
@@ -108,38 +118,26 @@ const Main = () => {
 		// 	)}
 		// </_Container>
 
-
 		<_Container>
-			<_header>
-				ss
-			</_header>
+			<_side></_side>
 			<_contentsContainer>
-
 				<_contents>
-					{favorite.map((v,index) => {
-					return (
-						<ContentsButton img={v.img} title={v.title}  text={v.text} key={index}/>
-					);
-				})}
+					{favorite.map((v, index) => {
+						return (
+							<ContentsButton
+								img={v.img}
+								title={v.title}
+								text={v.text}
+								key={index}
+							/>
+						);
+					})}
 				</_contents>
-				<_contents>
-
-				</_contents>
-				<_contents>
-
-				</_contents>
-				<_contents>
-
-
-				</_contents>
+				<_contents></_contents>
+				<_contents></_contents>
+				<_contents></_contents>
 			</_contentsContainer>
-			<_footer>
-				ss
-			</_footer>
-
-
-
-
+			<_side></_side>
 		</_Container>
 	);
 };
