fe90a1064c4b937af4177e3aee85989b53f8f255
roberto6385 <roberto@netand.co.kr>
2021-12-02 15:39:24 +0900

    "css_update : [회사별 메인이미지 변경 , 메인페이지 반응형추가]"

---
+++ src/styles/responsive/settings.js
@@ -0,0 +1,20 @@
+export const size = {
+	mobileS: '320px',
+	mobileM: '375px',
+	mobileL: '425px',
+	Ipad: '768px',
+	IpadPro: '1366px',
+	laptopL: '1440px',
+	desktop: '2560px',
+};
+
+export const device = {
+	mobileS: `(min-width: ${size.mobileS})`,
+	mobileM: `(min-width: ${size.mobileM})`,
+	mobileL: `(min-width: ${size.mobileL})`,
+	Ipad: `(min-width: ${size.Ipad})`,
+	IpadPro: `(min-width: ${size.IpadPro})`,
+	laptopL: `(min-width: ${size.laptopL})`,
+	desktop: `(min-width: ${size.desktop})`,
+	desktopL: `(min-width: ${size.desktop})`,
+};
