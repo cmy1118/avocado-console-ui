15d0b0e404119759d10001af525948f4e37a364b
ambacc244 <ambacc244@gmail.co.kr>
2022-02-21 14:40:33 +0900

    feat: UserAccountProcessTemplate 컴포넌트 생성

---
+++ src/components/IAM/Policy/Components/Templates/UserAccountProcessTemplate.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+/**************************************************
+ * ambacc244 - 사용자 계정 처리 컴포넌트
+ **************************************************/
+const UserAccountProcessTemplate = () => {
+	return <div></div>;
+};
+export default UserAccountProcessTemplate;
