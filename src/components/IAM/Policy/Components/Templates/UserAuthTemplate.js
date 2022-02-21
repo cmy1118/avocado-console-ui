01654d7b68f6025f15b49f4109c5b4e6d0b62f7f
ambacc244 <ambacc244@gmail.co.kr>
2022-02-21 14:35:44 +0900

    feat: UserAuthTemplate 컴포넌트의 text 오브젝트 형채로 작성

--- src/components/IAM/Policy/Components/Templates/UserAuthTemplate.js
+++ src/components/IAM/Policy/Components/Templates/UserAuthTemplate.js
@@ -4,6 +4,128 @@ import {FoldableContainer} from '../../../../../styles/components/iam/iam';
 import TableFold from '../../../../Table/Options/TableFold';
 import CheckBox from '../../../../RecycleComponents/New/CheckBox';
 
+const text = {
+	title: '사용자 인증',
+	contents: {
+		deviceAuth: {
+			title: '단말기 인증',
+			description: ['단말기 인증이 필요한 애플리 케이션을 설정합니다.'],
+			contents: {
+				usage: {
+					title: '사용 여부',
+					options: {use: '사용함', nonuse: '사용 안함'},
+				},
+				controlApplication: {
+					title: '제어 어플리케이션',
+					options: {
+						managementConsole: 'Management Console',
+						webTerminal: 'WebTerminal',
+					},
+				},
+			},
+		},
+		mfa: {
+			title: 'MFA 인증',
+			description: [
+				'기본 인증 이후 추가 인증을 설정할 수 있으며, 최대 3차 까지 가능합니다.',
+				'인증 수단은 중복 설정 할 수 없습니다.',
+			],
+			contents: {
+				usage: {
+					title: '사용 여부',
+					options: {use: '사용함', nonuse: '사용 안함'},
+				},
+				additionalCertification1: {
+					title: '1차 추가 인증',
+					certification: {use: '인증 함', nonuse: '인증 안함'},
+					required: {required: '필수', optional: '선택', no: '없음'},
+					checkBoxOptions: {
+						mail: '인증번호(Mail)',
+						sms: '인증번호(SMS)',
+						kakao: '인증번호(Kakao)',
+					},
+				},
+				additionalCertification2: {
+					title: '1차 추가 인증',
+					certification: {use: '인증 함', nonuse: '인증 안함'},
+					required: {required: '필수', optional: '선택', no: '없음'},
+					checkBoxOptions: {
+						mail: '인증번호(Mail)',
+						sms: '인증번호(SMS)',
+						kakao: '인증번호(Kakao)',
+					},
+				},
+				additionalCertification3: {
+					title: '1차 추가 인증',
+					certification: {use: '인증 함', nonuse: '인증 안함'},
+					required: {required: '필수', optional: '선택', no: '없음'},
+					authmethod: {
+						mail: '인증번호(Mail)',
+						sms: '인증번호(SMS)',
+						kakao: '인증번호(Kakao)',
+					},
+				},
+			},
+		},
+		failOver: {
+			title: 'Fail Over',
+			description: [
+				'인증수단의 문제로 실패시 다른 인증수단으로 전환 하여 인증을 진행합니다.',
+			],
+			contents: {
+				usage: {
+					title: '사용 여부',
+					options: {use: '사용함', nonuse: '사용 안함'},
+				},
+				basicAuth: {
+					title: '기본 인증',
+					options: {
+						no: '없음',
+						mail: '인증번호(Mail)',
+						sms: '인증번호(SMS)',
+						kakao: '인증번호(Kakao)',
+					},
+				},
+				mfa: {
+					title: '기본 인증',
+					options: {
+						no: '없음',
+						mail: '인증번호(Mail)',
+						sms: '인증번호(SMS)',
+						kakao: '인증번호(Kakao)',
+					},
+				},
+			},
+		},
+		identityVerification: {
+			title: '본인 확인 인증',
+			description: [
+				'화면 보호 해제  본인임을 확인하는 인증 절차를 설정 합니다.',
+				'입력 대기 시간은 최대 180초(3분)을 초과 할 수 없습니다.',
+			],
+			contents: {
+				usage: {
+					title: '사용 여부',
+					options: {use: '사용함', nonuse: '사용 안함'},
+				},
+				authType: {
+					title: '확인 유형',
+					options: {
+						no: 'ID/Password',
+						mail: '인증번호(Mail)',
+						sms: '인증번호(SMS)',
+						kakao: '인증번호(Kakao)',
+					},
+				},
+				mfa: {
+					title: '입력 대기 시간(초)',
+					sec: '초',
+				},
+			},
+		},
+	},
+};
+
 /**************************************************
  * ambacc244 - 사용자 인증 템플릿 컴포넌트
  **************************************************/
