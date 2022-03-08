6637131533ecb681c9c35669219441f897b04084
roberto6385 <roberto@netand.co.kr>
2022-03-08 12:13:13 +0900

    refactor : 사용자 관리 권한 - api response 데이터 처리 모듈화

--- src/components/IAM/Policy/Components/Templates/UserManagement.js
+++ src/components/IAM/Policy/Components/Templates/UserManagement.js
@@ -1,95 +1,81 @@
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
-import Table from '../../../../Table/Table';
-import {TableMode} from '../../../../../Constants/Table/mode';
-import checkboxColumn from '../../../../Table/tableCheckboxColumn';
-import {tableKeys} from '../../../../../Constants/Table/keys';
-import TableAllCheckbox from '../../../../Table/Options/TableAllCheckbox';
-import RowCheckbox from "../../../../RecycleComponents/rowCheckbox";
-import IAM_USER from "../../../../../reducers/api/IAM/User/User/user";
-import {useDispatch} from "react-redux";
-import IAM_POLICY_ACTION_TEMPLATE from "../../../../../reducers/api/IAM/Policy/ActionTemplate/actionTemplate";
-import {ColDiv} from "../../../../../styles/components/style";
+import React, {memo, useEffect, useState} from 'react';
+import RowCheckbox from '../../../../RecycleComponents/rowCheckbox';
+import {useDispatch} from 'react-redux';
+import IAM_POLICY_ACTION_TEMPLATE from '../../../../../reducers/api/IAM/Policy/ActionTemplate/actionTemplate';
+import {ColDiv} from '../../../../../styles/components/style';
+import {filterPropObj, objArrUnion} from '../../../../../utils/dataFitering';
 
 const constants = {
 	main: '사용자 관리 권한',
 	title: '',
 	templates: {},
-	templatesId :'KR-2020-0001:202202:0001'
+	templatesId: 'KR-2020-0001:202202:0001',
+	//체크박스 컬럼 정보
+	column: [
+		'전체권한',
+		'추가(생성)',
+		'수정',
+		'삭제',
+		'조회',
+		'부여',
+		'회수',
+		'설명',
+	],
+	//체크박스 action event 정보
+	action: ['created', 'updated', 'deleted', 'read', 'revoked'],
 };
-
-
 
 const UserManagement = () => {
 	const dispatch = useDispatch();
-	const [dataLists,setDataLists] =useState([]);
+	const [dataLists, setDataLists] = useState([]);
+	// 컬럼 에대한 action 정보
+	const tempDataLists = [
+		{action: 'create', data: false},
+		{action: 'read', data: false},
+		{action: 'update', data: false},
+		{action: 'delete', data: false},
+		{action: 'find', data: false},
+	];
 
-	// const dataLists = [
-	// 	{id : 1, data : "create"},
-	// 	{id : 2, data : "read"},
-	// 	{id : 3, data : "update"},
-	// 	{id : 4, data : "delete"},
-	// ]
-
-	const column =['전체권한','추가(생성)','수정','삭제','조회','부여','회수','설명']
-	const tempColumn =['created','updated','deleted','read','revoked']
-	const tempDataLists =[
-		{ action: 'create', data: false },
-		{ action: 'read', data: false },
-		{ action: 'update', data: false },
-		{ action: 'delete', data: false },
-		{ action: 'find', data: false },
- ]
-
-	//렌더링시 권한 템플릿 상세 정보를 조회
+	//렌더링시 체크박스 정보 조회
 	useEffect(() => {
 		const res = dispatch(
 			IAM_POLICY_ACTION_TEMPLATE.asyncAction.findAllAction({
 				range: 'elements=0-50',
-				templateId : constants.templatesId
+				templateId: constants.templatesId,
 			}),
-		).unwrap()
-			.then(res =>{
-				//모듈화 예정 ...
-				let filteredDataList =[]
-				const newData= res['data'].map(v=>{
-					let istrue;
-					 istrue = filteredDataList.filter(s=>{
-						return v['resource'] === s['resource']
-					})
-					if(filteredDataList[0] && istrue[0]){
-						const index = filteredDataList.findIndex(item => {
-							return item.resource === v['resource'];
-						});
-						filteredDataList[index].data.push(v)
-					}else{
-						filteredDataList.push({resource:v.resource , data: [v]})
-					}
-				})
-				const arr =[]
-				const result = filteredDataList.map(v=>{
-					const list = v['data'].map(s=>s.action)
-					const duplicatedData =tempDataLists.filter(item=>
-						!list.includes(item['action'])
-					)
-					const newData= v['data'].concat(duplicatedData)
-					const newObject ={resource:v.resource , data:newData}
-					arr.push(newObject)
-				})
-				console.log('Api-dataList:',arr)
-				setDataLists(arr)
-
-
-			})
+		)
+			.unwrap()
+			.then((res) => {
+				//배열 객체 프로퍼티 분리
+				const filteredDataList = filterPropObj(
+					res.data,
+					'resource',
+					'data',
+				);
+				//기존 temp배열 대상 배열객체 합치기
+				const result = objArrUnion(
+					filteredDataList,
+					tempDataLists,
+					'data',
+					'resource',
+					'action',
+				);
+				setDataLists(result);
+			});
 	}, [dispatch]);
 
 	return (
-
 		<div>
 			<ColDiv padding={'0px 0px 0px 25%'} width={'100%'}>
-			{column}
+				{constants.column}
 			</ColDiv>
-			{dataLists.map((item,index)=>(
-				<RowCheckbox title={item.resource} dataLists={item.data} key={index}/>
+			{dataLists.map((item, index) => (
+				<RowCheckbox
+					title={item.resource}
+					dataLists={item.data}
+					key={index}
+				/>
 			))}
 		</div>
 	);
--- src/utils/dataFitering.js
+++ src/utils/dataFitering.js
@@ -18,3 +18,59 @@ export function checkArraysIsUniqueHasDuplicates(arrA, arrB, value) {
 		return true;
 	}
 }
+
+/***********************************************************************************
+ * 배열 객체 프로퍼티 분리
+ * ex) [ {data:[{id:1, name:'홍길동' ...}]}] => [{id: 1 , data:[{id:1, name:'홍길동' ...}]]
+ * array : 배열객체
+ * prop: 분리할 프로퍼티
+ * target: 분리된 프로퍼티 저장 이름
+ ***********************************************************************************/
+export function filterPropObj(array, prop, target) {
+	let filterPropObject = [];
+	array.map((v) => {
+		let istrue;
+		istrue = filterPropObject.filter((s) => {
+			return v[prop] === s[prop];
+		});
+		if (filterPropObject[0] && istrue[0]) {
+			const index = filterPropObject.findIndex((item) => {
+				return item[prop] === v[prop];
+			});
+			filterPropObject[index][target].push(v);
+		} else {
+			let object = {};
+			object[prop] = v[prop];
+			object[target] = [v];
+			filterPropObject.push(object);
+		}
+	});
+	console.log('filterPropObject:', filterPropObject);
+	return filterPropObject;
+}
+
+/***********************************************************************************
+ * 배열객체 합치기 - 조건 합집합(기존의 원소 우선)
+ *
+ * array : 배열객체 (기존 배열)
+ * prop: 기존배열 prop
+ * prop2: 기존배열 prop2
+ * target: 조건이 되는 prop
+ ***********************************************************************************/
+export function objArrUnion(array, tempData, prop, prop2, target) {
+	let result = [];
+	array.map((v) => {
+		const list = v[prop].map((s) => s[target]);
+		const duplicatedData = tempData.filter(
+			(item) => !list.includes(item[target]),
+		);
+		const newData = v[prop].concat(duplicatedData);
+		let newObject = {};
+		newObject[prop2] = v[prop2];
+		newObject[prop] = newData;
+		// const newObject = {resource: v.resource, data: newData};
+		result.push(newObject);
+	});
+	console.log('result:', result);
+	return result;
+}
