4e81edc6a082eb64ce3b8d96e8aa1f9420255d6b
roberto6385 <roberto@netand.co.kr>
2022-03-15 15:36:10 +0900

    chore : IAM - 정책 생성 : action 생성,정책에 action 연결 수정

--- src/components/DialogBoxs/Preview/PolicyPreviewDialogBox.js
+++ src/components/DialogBoxs/Preview/PolicyPreviewDialogBox.js
@@ -47,77 +47,90 @@ const PolicyPreviewDialogBox = ({isOpened, setIsOpened, formData}) => {
 			// 올바른 step 아래에 disatch 작성 해주시면 감사하겠습니다.
 
 			//step1: 정책 생성
-			let policyId = null;
-			dispatch(
-				IAM_POLICY_MANAGEMENT_POLICIES.asyncAction.createPolicyAction({
-					name: formData.name,
-					description: formData.description,
-					type: policyManageTypes.Client,
-					controlTypes: [controlTypes.RBAC],
-					maxGrants: 5,
-				}),
-			)
-				.unwrap()
-				.then((data) => {
-					console.log('정책 아이디: ', data.id);
-					policyId = data.id;
-				});
+			// let policyId = null;
+			// dispatch(
+			// 	IAM_POLICY_MANAGEMENT_POLICIES.asyncAction.createPolicyAction({
+			// 		name: formData.name,
+			// 		description: formData.description,
+			// 		type: policyManageTypes.Client,
+			// 		controlTypes: [controlTypes.RBAC],
+			// 		maxGrants: 5,
+			// 	}),
+			// )
+			// 	.unwrap()
+			// 	.then((data) => {
+			// 		console.log('정책 아이디: ', data.id);
+			// 		policyId = data.id;
+			// 	});
 
 			//step2-1: action 생성
+			/**************************************************
+			 * reoberto - action 생성
+			 ***************************************************/
 			// if(actionTemplates[0]){
 			// 	actionTemplates.forEach(v=>{
-			// 		console.log('...actionTemplates[v],:',...actionTemplates[v],)
 			// 		dispatch(
-			// 			IAM_ACTION_MANAGEMENT_TEMPLATE.asyncAction.createAction(
-			// 				{...actionTemplates[v],}
-			// 			),
+			// 			IAM_ACTION_MANAGEMENT_TEMPLATE.asyncAction.createAction({
+			// 				name: v.name,
+			// 				description: v.description,
+			// 				details: v.details,
+			// 			}),
 			// 		).then((res)=>{
 			// 			console.log('권한 생성 res:',res)
 			// 		})
 			// 	})
 			// }
+			// ******************************************************
+
 
 			//step2-2: 정책 action 연결
+			/**************************************************
+			 * reoberto - 정책에 action 연결
+			 ***************************************************/
+			//:TODO 정책 action api 작업중 완료시 적용예정
 			// dispatch(
 			// 	IAM_POLICY_MANAGEMENT_ACTION_TEMPLATE.asyncAction.joinAction(
-			// 		iamTemplateIdList,
+			//   ,,,
 			// 	),
+			// ******************************************************
+
 
 			//step3-1: rule 생성
-			let order = 1;
-			const templateList = [];
-			for (const v in ruleTemplates) {
-				dispatch(
-					IAM_RULE_MANAGEMENT_TEMPLATE.asyncAction.createRuleTemplateAction(
-						{
-							...ruleTemplates[v],
-							attributes: ruleTemplates[
-								v
-							].attributes.map((data) => JSON.stringify(data)),
-						},
-					),
-				)
-					.unwrap()
-					.then((data) => {
-						console.log('rule 아이디: ', data.id);
-						templateList.push({
-							policyId: policyId,
-							templateId: data.id,
-							order: order++,
-						});
-					});
-			}
+			// let order = 1;
+			// const templateList = [];
+			// for (const v in ruleTemplates) {
+			// 	dispatch(
+			// 		IAM_RULE_MANAGEMENT_TEMPLATE.asyncAction.createRuleTemplateAction(
+			// 			{
+			// 				...ruleTemplates[v],
+			// 				attributes: ruleTemplates[
+			// 					v
+			// 				].attributes.map((data) => JSON.stringify(data)),
+			// 			},
+			// 		),
+			// 	)
+			// 		.unwrap()
+			// 		.then((data) => {
+			// 			console.log('rule 아이디: ', data.id);
+			// 			templateList.push({
+			// 				policyId: policyId,
+			// 				templateId: data.id,
+			// 				order: order++,
+			// 			});
+			// 		});
+			// }
+
 
 			//step3-2: 정책 rule 연결
 			/**************************************************
 			 * seob - 정책 rule 연결
 			 ***************************************************/
-			templateList.length === ruleTemplates.length &&
-				dispatch(
-					IAM_POLICY_MANAGEMENT_RULE_TEMPLATE.asyncAction.joinAction(
-						templateList,
-					),
-				);
+			// templateList.length === ruleTemplates.length &&
+			// 	dispatch(
+			// 		IAM_POLICY_MANAGEMENT_RULE_TEMPLATE.asyncAction.joinAction(
+			// 			templateList,
+			// 		),
+			// 	);
 			// ******************************************************
 		}
 	}, [formData, dispatch, ruleTemplates]);
